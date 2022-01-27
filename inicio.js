7aa0f64425d7dd632f50c3dd049ce9bf1d7e6968
jhenriquefreire <jh.henriquefreire@gmail.com>
2022-01-27 15:03:05 -0300

    Primeiro commit do repo

---
+++ inicio.js
@@ -0,0 +1,3 @@
+console.log('Hello World!');
+
+let numerodaSorte = 69;
---
+++ usuario.js
@@ -0,0 +1,13 @@
+let mariazinha = {
+    nome: 'Mariazinha',
+    idade: 25,
+    estado: 'Paraná'
+}
+
+let joaozinho = {
+    nome: 'Joãozinho',
+    idade: 22,
+    estado: 'Rio de Janeiro'
+}
+
+let nome = [joaozinho.nome, mariazinha.nome]
